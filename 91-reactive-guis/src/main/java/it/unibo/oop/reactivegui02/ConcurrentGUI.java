f0812348a7be12c5845e5c06349a2d3a700681d6
Daniele Muffato <daniele.muffato@studio.unibo.it>
2022-11-22 15:44:15 +0100

    1 -> 2 -> part3

--- 91-reactive-guis/src/main/java/it/unibo/oop/reactivegui02/ConcurrentGUI.java
+++ 91-reactive-guis/src/main/java/it/unibo/oop/reactivegui02/ConcurrentGUI.java
@@ -2,6 +2,7 @@ package it.unibo.oop.reactivegui02;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.lang.reflect.InvocationTargetException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -30,8 +31,11 @@ public final class ConcurrentGUI extends JFrame {
             while (counting) {
                 try {
                     this.counter = (this.dirIsUp) ? this.counter + 1 : this.counter - 1;
-                    final String text = (double)this.counter / 100 + "";
-                    SwingUtilities.invokeAndWait(() -> this.display.setText(text));
+                    final String text = String.format("%.2f", (double) this.counter / 100);
+                    SwingUtilities.invokeAndWait(() -> {
+                        this.display.setText(text);
+                        this.pack();
+                    });
                     Thread.sleep(10);
                 } catch (Throwable e) {
                     System.exit(1);
@@ -44,6 +48,25 @@ public final class ConcurrentGUI extends JFrame {
         panel.add(stop);
         this.getContentPane().add(panel);
         this.pack();
+        this.setLocationByPlatform(true);
         this.setVisible(true);
+        this.up.addActionListener(this::up);
+        this.down.addActionListener(this::down);
+        this.stop.addActionListener(this::disable);
+    }
+
+    private void up(Object o) {
+        this.dirIsUp = true;
+    }
+
+    private void down(Object o) {
+        this.dirIsUp = false;
+    }
+
+    private void disable(Object o) {
+        this.counting = false;
+        this.up.setEnabled(false);
+        this.down.setEnabled(false);
+        this.stop.setEnabled(false);
     }
 }
