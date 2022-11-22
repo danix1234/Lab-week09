99f62f0a7249478fde762ce1afaab1d853360843
Daniele Muffato <daniele.muffato@studio.unibo.it>
2022-11-22 15:48:16 +0100

    2 -> init

--- 91-reactive-guis/src/main/java/it/unibo/oop/reactivegui03/AnotherConcurrentGUI.java
+++ 91-reactive-guis/src/main/java/it/unibo/oop/reactivegui03/AnotherConcurrentGUI.java
@@ -18,4 +18,60 @@ import javax.swing.SwingUtilities;
  */
 @SuppressWarnings("unused")
 public final class AnotherConcurrentGUI extends JFrame {
+
+    private final JPanel panel = new JPanel();
+    private final JButton up = new JButton("up");
+    private final JButton down = new JButton("down");
+    private final JButton stop = new JButton("stop");
+    private final JLabel display = new JLabel();
+    private int counter = 0;
+    private volatile boolean counting = true;
+    private volatile boolean dirIsUp = true;
+
+    public AnotherConcurrentGUI() {
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        display.setText(Integer.toString(counter));
+        new Thread(() -> {
+            while (counting) {
+                try {
+                    this.counter = (this.dirIsUp) ? this.counter + 1 : this.counter - 1;
+                    final String text = String.format("%.2f", (double) this.counter / 100);
+                    SwingUtilities.invokeAndWait(() -> {
+                        this.display.setText(text);
+                        this.pack();
+                    });
+                    Thread.sleep(10);
+                } catch (Throwable e) {
+                    System.exit(1);
+                }
+            }
+        }).start();
+        panel.add(display);
+        panel.add(up);
+        panel.add(down);
+        panel.add(stop);
+        this.getContentPane().add(panel);
+        this.pack();
+        this.setLocationByPlatform(true);
+        this.setVisible(true);
+        this.up.addActionListener(this::up);
+        this.down.addActionListener(this::down);
+        this.stop.addActionListener(this::disable);
+    }
+
+    private void up(Object o) {
+        this.dirIsUp = true;
+    }
+
+    private void down(Object o) {
+        this.dirIsUp = false;
+    }
+
+    private void disable(Object o) {
+        this.counting = false;
+        this.up.setEnabled(false);
+        this.down.setEnabled(false);
+        this.stop.setEnabled(false);
+    }
+
 }
